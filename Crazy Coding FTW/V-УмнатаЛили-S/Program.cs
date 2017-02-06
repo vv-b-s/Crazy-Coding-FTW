df8a9ae54f8699a0918c2bf3fefad5d07bd3664f
Valentin Vasilev <brainstew_x_x@outlook.com>
2017-02-06 15:52:53 +0200

    V-УмнатаЛили-S

---
+++ Crazy Coding FTW/V-УмнатаЛили-S/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+class Program
+{
+
+    static void Main()
+    {
+        int age = (int)ReadNum();
+        double washingMachine = (double)ReadNum();
+        int toyPrice = (int)ReadNum();
+
+        int toysGained = (int)Math.Ceiling((double)age / 2);
+        int timesMoney = age / 2;
+
+        double money = toysGained * toyPrice - timesMoney;
+        for (int i = 0, j = 1; i < timesMoney; i++, j++)
+            money += j * 10;
+        WriteLine((money >= washingMachine) ? $"Yes! {Math.Round(money - washingMachine, 2):0.00}" : $"No! {Math.Round(washingMachine - money, 2):0.00}");
+    }
+    static decimal ReadNum()
+    {
+        string input = ReadLine();
+        decimal output;
+        decimal.TryParse(input, out output);
+        return output;
+    }
+}
