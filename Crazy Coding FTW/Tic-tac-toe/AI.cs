cfadb128451fc7119f99d46fceff54367d64c86b
Nikolay Lachev <nikolaylachev@gmail.com>
2017-02-07 14:56:56 +0200

    AI

--- Crazy Coding FTW/Tic-tac-toe/AI.cs
+++ Crazy Coding FTW/Tic-tac-toe/AI.cs
@@ -8,6 +8,19 @@ namespace AI
 {
     public class AI
     {
-        
+        static object[] HasX = new object ;
+        public static int[] PlaceO(string [,] X)
+        {
+            for (int i = 0; i < X.GetLength(0); i++)
+            {
+                for (int j = 0; j < X.GetLength(1); j++)
+                {
+                    if (X[i,j]=="X")
+                    {
+
+                    }
+                }
+            }
+        }
     }
 }
