a8056bd1319c635730ec30fb31f4c6cba0887ce4
Valentin Vasilev <brainstew_x_x@outlook.com>
2017-02-07 15:38:14 +0200

    Improved

--- Crazy Coding FTW/Tic-tac-toe/AI.cs
+++ Crazy Coding FTW/Tic-tac-toe/AI.cs
@@ -8,19 +8,70 @@ namespace AI
 {
     public class AI
     {
-        static object[] HasX = new object ;
-        public static int[] PlaceO(string [,] X)
-        {
-            for (int i = 0; i < X.GetLength(0); i++)
-            {
-                for (int j = 0; j < X.GetLength(1); j++)
-                {
-                    if (X[i,j]=="X")
-                    {
+        static int XesCount = 0;
+        static int OesCount = 0;
+        static int EmptyCount = 0;
 
+        static Random rand = new Random();
+        static bool[,] hasX =
+        {
+            {false,false,false },
+            {false,false,false },
+            {false,false,false }
+        };
+
+        static bool[,] hasO =
+        {
+            {false,false,false },
+            {false,false,false },
+            {false,false,false }
+        };
+
+        static bool[,] empty =
+        {
+            {false,false,false },
+            {false,false,false },
+            {false,false,false }
+        };
+
+        public static void Check(string[,] XO)
+        {
+            for (int i = 0; i < XO.GetLength(0); i++)
+                for (int j = 0; j < XO.GetLength(1); j++)
+                    if (XO[i, j] == "X")
+                    {
+                        hasX[i, j] = true;
+                        XesCount++;
                     }
+                    else if (XO[i, j] == "O")
+                    {
+                        hasO[i, j] = true;
+                        OesCount++;
+                    }
+                    else
+                    {
+                        empty[i, j] = true;
+                        EmptyCount++;
+                    }
+        }
+
+        public static int[] GenerateO()
+        {
+            int[] output = new int[2];
+            if(OesCount==1)
+            {
+                for(;;)
+                {
+                    output[0] = rand.Next(3);
+                    output[1] = rand.Next(3);
+
+                    if (hasX[output[0], output[1]] == true)
+                        continue;
+                    else return output;
                 }
             }
+            return output;
         }
+
     }
 }
--- Crazy Coding FTW/Tic-tac-toe/Form1.cs
+++ Crazy Coding FTW/Tic-tac-toe/Form1.cs
@@ -36,55 +36,78 @@ namespace Tic_tac_toe
             AccessBT(position[0], position[1]).Text = "X";
             AccessBT(position[0], position[1]).Enabled = false;
         }
+
+        private void PlaceO()
+        {
+            string[,] bt =
+            {
+                {bt1.Text,bt2.Text,bt3.Text },
+                {bt4.Text,bt5.Text,bt6.Text },
+                {bt7.Text,bt8.Text,bt9.Text },
+            };
+            AI.AI.Check(bt);
+            int[] pastePosition = AI.AI.GenerateO();
+            AccessBT(pastePosition[0], pastePosition[1]).Text = "O";
+            AccessBT(pastePosition[0], pastePosition[1]).Enabled = false;
+        }
         private void bt1_Click(object sender, EventArgs e)
         {
             BtClick(0, 0);
+            PlaceO();
         }
 
         private void bt2_Click(object sender, EventArgs e)
         {
             BtClick(0, 1);
+            PlaceO();
         }
 
         private void bt3_Click(object sender, EventArgs e)
         {
             BtClick(0, 2);
+            PlaceO();
         }
 
         private void bt4_Click(object sender, EventArgs e)
         {
             BtClick(1, 0);
+            PlaceO();
         }
 
         private void bt5_Click(object sender, EventArgs e)
         {
             BtClick(1, 1);
+            PlaceO();
         }
 
         private void bt6_Click(object sender, EventArgs e)
         {
             BtClick(1, 2);
+            PlaceO();
         }
 
         private void bt7_Click(object sender, EventArgs e)
         {
             BtClick(2, 0);
+            PlaceO();
         }
 
         private void bt8_Click(object sender, EventArgs e)
         {
             BtClick(2, 1);
+            PlaceO();
         }
 
         private void bt9_Click(object sender, EventArgs e)
         {
             BtClick(2, 2);
+            PlaceO();
         }
 
         private void ngBT_Click(object sender, EventArgs e)
         {
-            for(int i = 0;i<3;i++)
-                for(int j =0;j<3;j++)
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
                 {
                     AccessBT(i, j).Text = "";
                     AccessBT(i, j).Enabled = true;
