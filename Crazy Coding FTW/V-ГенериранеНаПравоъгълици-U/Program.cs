0fd0bb3328c3a9787767e8d2a7bebd768c8eace4
Valentin Vasilev <brainstew_x_x@outlook.com>
2017-02-12 14:15:13 +0200

    V-ГенериранеНаПравоъгълници-S

--- Crazy Coding FTW/V-ГенериранеНаПравоъгълици-U/Program.cs
+++ Crazy Coding FTW/V-ГенериранеНаПравоъгълици-U/Program.cs
@@ -11,27 +11,24 @@ class Program
 
         bool isLimArea = false;
 
-        if (limArea > limit)
+        if (limit!=0)
         {
             for (int x2 = limit * -1 + 1; x2 <= limit; x2++)
                 for (int y2 = limit * -1 + 1; y2 <= limit; y2++)
                     for (int x1 = limit * -1; x1 < x2; x1++)
                         for (int y1 = limit * -1; y1 < y2; y1++)
                         {
-                            if (Area(x1, y1, x2, y2) >= limArea && x1 < x2 && y1 < y2)
+                            int side1 = Math.Max(x1, x2) - Math.Min(x1, x2);
+                            int side2 = Math.Max(y1, y2) - Math.Min(y1, y2);
+                            if(side1*side2 >= limArea && x1 < x2 && y1 < y2)
                             {
-                                WriteLine($"({x1}, {y1}) ({x2}, {y2}) -> {Area(x1, y1, x2, y2)}");
+                                WriteLine($"({x1}, {y1}) ({x2}, {y2}) -> {side2*side1}");
                                 isLimArea = true;
                             }
                         }
         }
-        if (!isLimArea || limit == 0 || limArea == 0)
+        if (!isLimArea || limit == 0)
             WriteLine("No");
     }
 
-    static int Area(params int[] p)
-    {
-        int[] side = { Math.Max(p[0], p[2]) - Math.Min(p[0], p[2]), Math.Max(p[1], p[3]) - Math.Min(p[1], p[3]) };
-        return side[0] * side[1];
-    }
 }
