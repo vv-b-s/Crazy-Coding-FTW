0b26620dd1e63faaf18bd913938c43c8761f30e1
Valentin Vasilev <brainstew_x_x@outlook.com>
2017-02-18 22:50:10 +0200

    V-NumberPyramid-S

---
+++ Crazy Coding FTW/V-NumberPyramid-S/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using static System.Console;
+
+class Program
+{
+    static void Main()
+    {
+        int n = (int)ReadNum();
+        for (int i = 1, j = 1; i <= n; j++)
+        {
+            for (int k = 0; k < j; k++)
+            {
+                Write(i + " ");
+                i++;
+                if (i > n)
+                    break;
+            }
+            WriteLine();
+        }
+    }
+
+
+    static decimal ReadNum()
+    {
+        string input = ReadLine();
+        decimal output;
+        decimal.TryParse(input, out output);
+        return output;
+    }
+}
